cceba0e85758156b9c9f6235cb78695460991011
broholens <broholens@github.com>
2018-11-22 12:04:33 +0800

    celery added

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,19 @@
-from gevent import monkey
-monkey.patch_all()
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-from model import insert
+from celery import Celery, platforms
+from newspaper import Article
+from model import insert_url, insert_txt
+from gevent import monkey
+monkey.patch_all()
 
+platforms.C_FORCE_ROOT = True
 
 app = Flask(__name__)
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/1'
+celery = Celery(app.name)
+celery.conf.update(app.config)
+
 api = Api(app)
 
 @app.after_request
@@ -16,14 +24,26 @@ def after_request(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
+@celery.task
+def insert_article_text(url):
+    try:
+        article = Article(url)
+        article.download()
+        article.parse()
+        txt = article.text
+    except:
+        txt = ''
+    insert_txt(url, txt)
+
 class Url(Resource):
     def get(self):
         url = request.args.get('url')
-        result = insert(url)
-        return jsonify({'result': result})
+        result = insert_url(url)
+        insert_article_text.delay(url)
+        return jsonify(result)
 
 api.add_resource(Url, '/api/insert')
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0')
+    app.run()
--- backend/model.py
+++ backend/model.py
@@ -1,20 +1,31 @@
+# -*- coding: utf-8 -*-
 from pymongo import MongoClient
+from utils import get_article_text
 
 client = MongoClient()
 db = client.articles
 collection = db.articles
-collection.ensure_index('url', unique=True)
+collection.create_index('url', unique=True)
 
+def insert_url(url):
+    try:
+        result = collection.update_one(
+            {'url': url},
+            # {'$set': {'txt': url}},
+            upsert=True
+        )
+        if result.matched_count > 0:
+            return {'type': 'warning', 'msg': 'url already exists!'}
+        return {'type': 'success', 'msg': 'url add successfully!'}
+    except:
+        return {'type': 'error', 'msg': 'faild to add url'}
 
-def insert(url):
+def insert_txt(url, txt):
     try:
         collection.update_one(
             {'url': url},
-            {'$set': {'url': url}},
+            {'$set': {'txt': txt}},
             upsert=True
         )
-        return True
     except:
-        return False
-
-
+        pass
